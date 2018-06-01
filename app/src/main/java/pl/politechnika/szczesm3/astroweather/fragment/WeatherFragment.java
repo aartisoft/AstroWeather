0c4e1adf0b9e59a4a4d288e88e7a598e1fd270fd
szczesnymateo <Pr0xinati0n>
2018-06-01 12:09:04 +0200

    init

---
+++ app/src/main/java/pl/politechnika/szczesm3/astroweather/fragment/WeatherFragment.java
@@ -0,0 +1,22 @@
+package pl.politechnika.szczesm3.astroweather.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class WeatherFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
